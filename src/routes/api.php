4cf0af0b7c265ae23327cdeb5f5a826b1c3a586c
Gilbert Elikplim kukah <kwamegilbert1114@gmail.com>
2026-05-29 14:31:48 +0000

    refactor: replace dynamic route loading with a white-list of active routing files

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,16 +1,29 @@
 <?php
 return function ($app): void {
-    // Dynamically load all versioned route files in the v1 directory
-    $routeDir = ROUTE . 'v1/';
-    $routeFiles = glob($routeDir . '*.php');
-    
-    if (is_array($routeFiles)) {
-        foreach ($routeFiles as $file) {
-            if (file_exists($file)) {
-                $routeLoader = require $file;
-                if (is_callable($routeLoader)) {
-                    $routeLoader($app);
-                }
+    // White-list of active routing files for the Constituency Development Hub
+    $activeRoutes = [
+        ROUTE . 'v1/AdminRoute.php',
+        ROUTE . 'v1/AgentRoute.php',
+        ROUTE . 'v1/AuthRoute.php',
+        ROUTE . 'v1/CategoryRoute.php',
+        ROUTE . 'v1/ConstituentRoute.php',
+        ROUTE . 'v1/ContentRoute.php',
+        ROUTE . 'v1/DevelopmentRoute.php',
+        ROUTE . 'v1/EmploymentRoute.php',
+        ROUTE . 'v1/IssueRoute.php',
+        ROUTE . 'v1/LocationRoute.php',
+        ROUTE . 'v1/OfficerRoute.php',
+        ROUTE . 'v1/SectorRoute.php',
+        ROUTE . 'v1/SubSectorRoute.php',
+        ROUTE . 'v1/UserRoute.php',
+        ROUTE . 'v1/UtilsRoute.php',
+    ];
+
+    foreach ($activeRoutes as $file) {
+        if (file_exists($file)) {
+            $routeLoader = require $file;
+            if (is_callable($routeLoader)) {
+                $routeLoader($app);
             }
         }
     }
